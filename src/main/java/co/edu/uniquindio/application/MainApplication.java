ca4cbbf85f007f32e44e1365ceb7a2e124df44ef
lyc4nthrope <osoriocristhian86@gmail.com>
2025-12-10 02:23:21 -0500

    Pruebas unitarias y de integración en Spring Boot completo4

--- src/main/java/co/edu/uniquindio/application/MainApplication.java
+++ src/main/java/co/edu/uniquindio/application/MainApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
 }
+
+//revisar el chat en tiempo real si sirve?
